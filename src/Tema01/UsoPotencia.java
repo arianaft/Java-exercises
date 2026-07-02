d77f891bc18fe333e8d5a15531dae8e8f9f9e843
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-07-02 17:27:26 +0200

    feat:uso de la clase pow

---
+++ src/Tema01/UsoPotencia.java
@@ -0,0 +1,11 @@
+package Tema01;
+
+public class UsoPotencia {
+    public static void main(String[] args) {
+
+        //Uso del método pow
+        int potencia =(int)Math.pow(25,560);
+        System.out.print(potencia);
+
+    }
+}
