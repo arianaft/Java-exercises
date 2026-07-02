42ca00f6f503c4551728368f346f842064dc26fa
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-07-02 17:14:13 +0200

    feat: uso de la clase Math

---
+++ src/Tema01/UsoMath.java
@@ -0,0 +1,10 @@
+package Tema01;
+
+public class UsoMath {
+    public static void main(String[] args) {
+        //Casting y uso de la clase Math
+        int raiz=(int)Math.sqrt(10);
+        System.out.println(raiz);
+
+    }
+}
