a23b221e1c0c612e5a3a8f5802f2a8d65b459d7a
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-07-07 18:08:48 +0200

    feat: Tema02 metods de String

---
+++ src/Tema02/ManipulacionCadena.java
@@ -0,0 +1,13 @@
+package Tema02;
+
+public class ManipulacionCadena {
+    public static void main(String[] args) {
+
+        //Método length , longitud de la cadena
+
+        String nombre = "Ariana";
+        nombre.length();
+        System.out.println(nombre);
+
+    }
+}
---
+++ src/Tema02/UsoCharat.java
@@ -0,0 +1,12 @@
+package Tema02;
+
+public class UsoCharat {
+    public static void main(String[] args) {
+
+        //Método charAt
+        String nombre="María";
+        System.out.println("Mi nombre tiene "+ nombre.length() + " letras");
+        System.out.println("La primera letra de mi nombre es "+ nombre.charAt(0));
+
+    }
+}
