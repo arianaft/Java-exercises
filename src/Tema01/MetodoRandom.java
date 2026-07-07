ae0cbfd8ad412fb815d33a951d25cda5ecd00a05
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-07-07 17:22:27 +0200

    feat: Uso método Random

---
+++ src/Tema01/MetodoRandom.java
@@ -0,0 +1,12 @@
+package Tema01;
+
+public class MetodoRandom {
+    public static void main(String[] args) {
+        //Generar un numero aleatorio entre 0 y 100
+
+        double numDoble =(int)(Math.random() *100);
+
+        System.out.print("Numero aleatorio: " + numDoble);
+
+    }
+}
