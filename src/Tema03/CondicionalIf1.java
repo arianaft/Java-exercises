5e55599fe78b871360b3459f0468af838062f969
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-07-15 13:47:59 +0200

    feat: añadir ejericio condicionales

---
+++ src/Tema03/CondicionalIf1.java
@@ -0,0 +1,22 @@
+package Tema03;
+
+import java.util.Scanner;
+
+public class CondicionalIf1 {
+    public static void main(String[] args) {
+
+        //Programa que indica mayoría de edad
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu edad");
+
+        int edad  = sc.nextInt();
+
+        if (edad <= 18){
+            System.out.println("Bienvenido a la  mayoría de edad");
+        }else {
+            System.out.println("Eres muy joven");
+        }
+    }
+
+}
